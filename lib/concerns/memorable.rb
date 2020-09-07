dd62ffabe024bd02d6dfb9d2c87d72f1a0e95011
Nancy Jackson <nbjackson23@gmail.com>
2020-09-07 23:46:41 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,22 @@
 module Memorable
-  def reset_all
-    @reset_all
-  end
-  
-  def count
-    @count
-  end
-  
-end
 
+  module ClassMethods
+
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+
+  end
+
+  module InstanceMethods
+
+    def initialize
+      self.class.all << self
+    end
+  end
+
+end
