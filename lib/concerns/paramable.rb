bc4e7410575047b1d6e659203e191aa68b9f0821
Nancy Jackson <nbjackson23@gmail.com>
2020-09-08 00:27:33 +0000

    Done.

---
+++ lib/concerns/paramable.rb
@@ -0,0 +1,8 @@
+module PARAMABLE
+
+  def to_param
+    name.downcase.gsub(' ', '-')
+    # binding.pry
+  end
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,43 @@
 require 'pry'
 
 class Song
-  attr_accessor :name
-  attr_reader :artist
+  
+  #extend memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include PARAMABLE
+  
+ attr_accessor :name, :artist
+  #attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  #def initialize
+   # @@songs << self
+  #end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  #def self.find_by_name(name)
+    #@@songs.detect{|a| a.name == name}
+  #end
 
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  #def self.reset_all
+   # self.all.clear
+  #end
 
-  def self.count
-    self.all.count
-  end
+  #def self.count
+   # self.all.count
+  #end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+   # name.downcase.gsub(' ', '-')
+  #end
 end
