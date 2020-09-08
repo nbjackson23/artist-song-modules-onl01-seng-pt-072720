23b68d0ad1def8da3f46d5d99667677371cbf553
Nancy Jackson <nbjackson23@gmail.com>
2020-09-08 00:09:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,10 +2,12 @@ require 'pry'
 
 class Artist
   
+   # extend Memorable
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
   include PARAMABLE
+  # include Initialize
   
   attr_accessor :name
   attr_reader :songs
